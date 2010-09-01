05ea35995e3eecb4ffc4e9a73e6446cef0ef4911
daku <dakshay@gmail.com>
2010-09-01 12:19:01 -0700

    moving back to the old way, since this is not a scalable approach (openeing hundreds of tabs, loading pages,  and closing them). The cache is also ephemeral so no guarantee what's a good time to update. Lots of pages take too long to load because of running background scripts so that's another problem to deal with as well.

--- getbm.js
+++ getbm.js
@@ -1,5 +1,4 @@
 document.body.bgColor='red';
 //alert(document.body.innerHTML);
 //alert(document.body.innerHTML);
-chrome.extension.sendRequest({method: 'store', arg: 'hello', 
-            arg2: document.body.innerHTML + document.head.innerHTML}, function(response) {});
+
