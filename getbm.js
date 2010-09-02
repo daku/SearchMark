80983b2632d639b1963d3aafb6072698a6edafd9
daku <dakshay@gmail.com>
2010-09-02 09:07:41 -0700

    Download and store content for all current bookmarks.

--- getbm.js
+++ getbm.js
@@ -2,4 +2,4 @@ document.body.bgColor='red';
 //alert(document.body.innerHTML);
 //alert(document.body.innerHTML);
 chrome.extension.sendRequest({method: 'store', arg: 'hello', 
-            arg2: document.body.innerHTML + document.head.innerHTML}, function(response) {});
+            arg2: document.body.innerHTML}, function(response) {});
